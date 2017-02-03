3d56a32f606e98c03aab2fefee5475d88ae05082
Mystic Weng <710120644@qq.com>
2017-02-03 19:29:43 +0800

    handle json array

--- client.js
+++ client.js
@@ -3,21 +3,34 @@ var request = require('request')
 const LZXF_TID = 33
 //完结动画
 const WZDH_TID = 32
-const PN_MAX = 9999999999
+const PN_MAX = 999999
 var pn = 1
-var total = PN_MAX 
-             
+var total_pn = PN_MAX 
+
+ //递归Request方法 pn+1             
 function handleRequest(){
     var request_url = setParamData(LZXF_TID,pn)
+    // console.log(request_url)
     request(request_url,function(error,response,body){
             if(response.statusCode==200){
-                console.log(request_url)
+                var resJson = JSON.parse(body)
+                // console.log(resJson)
+                var dataJson = resJson.data
+                if(pn==1){
+                    var pageObj = dataJson.page
+                    // console.log(pageObj)
+                    console.log('Total count: ' + pageObj.count)                  
+                    total_pn = Math.ceil(pageObj.count / pageObj.size)
+                    console.log('Total Page Number: ' + total_pn)
+                }
+                handleData(dataJson)
                 pn += 1
             }else{
-                throw error
+                console.log('Error log: ' + error)
+                return
             }
 
-            if(total!=PN_MAX && pn>total){
+            if(total_pn!=PN_MAX && pn>total_pn){
                 return
             }else{
                 handleRequest()
@@ -25,13 +38,13 @@ function handleRequest(){
     })
 }
 
-function handleData(){
-
+function handleData(data){
+    var dataArray = []
 }
 
 function setParamData(tid,pn){
-    var url = "http://api.bilibili.com/archive_rank/getarchiverankbypartion?type=jsonp&tid={tid}&pn={pn}"
-    return url.replace("{tid}",tid).replace("{pn}",pn)
+    var url = 'http://api.bilibili.com/archive_rank/getarchiverankbypartion?type=jsonp&tid={tid}&pn={pn}'
+    return url.replace('{tid}',tid).replace('{pn}',pn)
 }
 
 /*
